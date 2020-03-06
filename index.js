5b587dc1b8960f666f32d00140c4f6152a09f9f1
aliyasser20 <aliyasser20@hotmail.com>
2020-03-06 09:45:56 -0500

    completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = function(fromN, toN) {
+  let result = fromN;
+
+  if (fromN < toN) {
+    result += sum(fromN + 1, toN);
+  }
+
+  return result;
+};
 
 module.exports = sum;
+
+console.log(sum(3, 7));
